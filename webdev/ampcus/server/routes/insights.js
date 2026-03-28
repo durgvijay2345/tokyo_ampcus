3b8b5bdbf610289eca26aa4cf066e513907a33ed
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2026-03-28 23:04:30 +0530

    Delete webdev directory

--- webdev/ampcus/server/routes/insights.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const insightController = require('../controllers/insightController');
-
-
-router.get('/:analysisId', insightController.getInsightsByAnalysisId);
-
-module.exports = router;
