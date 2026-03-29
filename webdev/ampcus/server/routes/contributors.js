fafa141e1faf7f5fcc9249b132df4955241f1a51
Durgvijay Tiwari <durgvijaytiwari82@gmail.com>
2026-03-29 14:36:00 +0530

    update database.js

--- webdev/ampcus/server/routes/contributors.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contributorController = require('../controllers/contributorController');
-
-
-router.get('/:analysisId', contributorController.getContributorsByAnalysisId);
-
-module.exports = router;
